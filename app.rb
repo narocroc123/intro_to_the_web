da5314cf9c6e77ee8ea162ea8c4b8880fe0347bf
Fergus Corcoran <fergus.corcoran@gmail.com>
2020-01-07 15:52:06 +0000

    Adding Views

--- app.rb
+++ app.rb
@@ -3,20 +3,6 @@ require 'shotgun'
 
 set :session_secret, 'super secret'
 
-get '/' do
-  "Hello!"
-end
-
-get '/secret' do
-  "Secret"
-end
-
-get '/test' do
-  "test"
-end
-
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
